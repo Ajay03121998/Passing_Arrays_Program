b9bdc1fa910d8b4504afa227ac3e222e730a7f95
Muppidi-Ajay-Kumar <Muppidi.Ajay.Kumar@ibm.com>
2022-03-03 11:12:53 +0530

    Add project files.

---
+++ Passing_Arrays_Program/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passing_Arrays_Program
+{
+    internal class Program
+    {
+        static void Result(int[] arr)
+        {
+
+            // displaying the array elements
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Array Element: " + arr[i]);
+            }
+        }
+        static void Main()
+        {
+            // declaring an array
+            // and initializing it
+            int[] arr = { 10, 652, 122, 74, 69 };
+
+            // calling the method
+            Result(arr);
+            Array.Sort(arr);
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
